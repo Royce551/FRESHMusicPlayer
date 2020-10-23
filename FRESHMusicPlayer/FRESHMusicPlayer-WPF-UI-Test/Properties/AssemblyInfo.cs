e1593e4a870ccc432bbd912838809df0ed55bca9
Royce551 <poohwie2005@gmail.com>
2020-10-22 21:40:04 -0500

    Set up project for Squirrel

--- FRESHMusicPlayer/FRESHMusicPlayer-WPF-UI-Test/Properties/AssemblyInfo.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer-WPF-UI-Test/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("FRESHMusicPlayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Squid Grill")]
 [assembly: AssemblyProduct("FRESHMusicPlayer")]
-[assembly: AssemblyCopyright("Copyleft © Squid Grill 2020")]
+[assembly: AssemblyCopyright("Copyright © Squid Grill 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
+[assembly: AssemblyInformationalVersion("FRESHMusicPlayer Ver. 8")]
