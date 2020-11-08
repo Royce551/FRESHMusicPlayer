3d9848201b0f1c126dcb8eb5cb9a7eadad283a5e
Royce551 <poohwie2005@gmail.com>
2020-11-08 12:05:11 -0600

    Update FMP Core

--- FRESHMusicPlayer/FRESHMusicPlayer-WPF-UI-Test/Pages/QueueManagement/QueueEntry.xaml.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer-WPF-UI-Test/Pages/QueueManagement/QueueEntry.xaml.cs
@@ -1,19 +1,6 @@
-﻿using FRESHMusicPlayer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FRESHMusicPlayer.Pages
 {
@@ -34,6 +21,7 @@ namespace FRESHMusicPlayer.Pages
             {
                 TitleLabel.FontWeight = FontWeights.Bold;
                 ArtistAlbumLabel.FontWeight = FontWeights.Bold;
+                PositionLabel.Text = ">";
             }
         }
         private void UserControl_MouseEnter(object sender, MouseEventArgs e)
