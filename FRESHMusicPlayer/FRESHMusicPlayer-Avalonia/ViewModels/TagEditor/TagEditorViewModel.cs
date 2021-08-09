5ecf94d43611f3c54f2cf5d2bb86f5a0a1a7d8de
Royce551 <poohwie2005@gmail.com>
2021-08-09 16:29:16 -0500

    Basic cover art editing implementation

--- FRESHMusicPlayer/FRESHMusicPlayer-Avalonia/ViewModels/TagEditor/TagEditorViewModel.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer-Avalonia/ViewModels/TagEditor/TagEditorViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ATL;
 using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using Avalonia.Metadata;
 using FRESHMusicPlayer.Handlers;
 using ReactiveUI;
+using Drawing = SixLabors.ImageSharp;
 
 namespace FRESHMusicPlayer.ViewModels.TagEditor
 {
@@ -108,6 +113,109 @@ namespace FRESHMusicPlayer.ViewModels.TagEditor
             }
         }
 
+        private Bitmap coverArt;
+        public Bitmap CoverArt
+        {
+            get => coverArt;
+            set => this.RaiseAndSetIfChanged(ref coverArt, value);
+        }
+        private string coverArtLabel;
+        public string CoverArtLabel
+        {
+            get => coverArtLabel;
+            set => this.RaiseAndSetIfChanged(ref coverArtLabel, value);
+        }
+
+        public ObservableCollection<string> AvailableCoverArts { get; set; } = new(); // this is used for display purposes; just 1-2-3
+        public ObservableCollection<PictureInfo> CoverArts { get; set; } = new(); // actual available cover arts for internal purposes
+
+        private int selectedCoverArt = -1;
+        public int SelectedCoverArt
+        {
+            get => selectedCoverArt;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref selectedCoverArt, value);
+                ChangeCoverArt();
+            }
+        }
+
+        public async void ImportCoverArtCommand()
+        {
+            var dialog = new OpenFileDialog()
+            {
+                Filters = new List<FileDialogFilter>
+                {
+                    new FileDialogFilter()
+                    {
+                        Name = "Image Files",
+                        Extensions = new List<string>() { "png", "jpg" }
+                    },
+                    new FileDialogFilter()
+                    {
+                        Name = "Other",
+                        Extensions = new List<string>() { "*" }
+                    }
+                },
+                AllowMultiple = true
+            };
+            var files = await dialog.ShowAsync(Window);
+            CoverArts[SelectedCoverArt] = PictureInfo.fromBinaryData(File.ReadAllBytes(files[0]), CoverArts[SelectedCoverArt].PicType);
+            ChangeCoverArt();
+            UnsavedChanges = true;
+        }
+        [DependsOn(nameof(FilePaths))]
+        public bool CanImportCoverArtCommand(object parameter) => /*FilePaths.Any();*/true;
+
+        public void AddCoverArtCommand()
+        {
+            CoverArts.Add(new PictureInfo(PictureInfo.PIC_TYPE.Front));
+            if (SelectedCoverArt == -1) SelectedCoverArt = 0;
+            UpdateCoverArtSelector();
+            ChangeCoverArt();
+            UnsavedChanges = true;
+        }
+
+        public void RemoveCoverArtCommand()
+        {
+            CoverArts.RemoveAt(SelectedCoverArt);
+            UpdateCoverArtSelector();
+            ChangeCoverArt();
+            UnsavedChanges = true;
+        }
+        [DependsOn(nameof(AvailableCoverArts))]
+        public bool CanRemoveCoverArtCommand(object parameter) => /*AvailableCoverArts.Any();*/true;
+
+        public void ChangeCoverArt()
+        {
+            if (SelectedCoverArt == -1) SelectedCoverArt = 0;
+            var currentCover = CoverArts[SelectedCoverArt];
+            if (currentCover.PictureData is null)
+            {
+                CoverArtLabel = "No data for this cover";
+                CoverArt = null;
+                return;
+            }
+            var currentCoverImage = Drawing.Image.Identify(new MemoryStream(currentCover.PictureData), out var coverFormat);
+            CoverArt = new Bitmap(new MemoryStream(currentCover.PictureData));
+            CoverArtLabel =
+                $"{currentCoverImage.Width}x{currentCoverImage.Height}\n" +
+                $"{coverFormat.Name} Image\n" +
+                $"{currentCover.PicType}";
+        }
+        public void UpdateCoverArtSelector()
+        {
+            var selectedIndex = SelectedCoverArt;
+            AvailableCoverArts.Clear();
+            int i = 1;
+            foreach (var cover in CoverArts)
+            {
+                AvailableCoverArts.Add(i.ToString());
+                i++;
+            }
+            if (SelectedCoverArt > AvailableCoverArts.Count) SelectedCoverArt = AvailableCoverArts.Count;
+        }
+
         public List<string> FilePaths { get; set; } = new();
 
         private const string windowName = "FRESHMusicPlayer Tag Editor";
@@ -149,6 +257,8 @@ namespace FRESHMusicPlayer.ViewModels.TagEditor
         public void Initialize(List<string> filePaths)
         {
             FilePaths = filePaths;
+            AvailableCoverArts.Clear();
+            CoverArts.Clear();
             this.RaisePropertyChanged(nameof(WindowTitle));
             foreach (var path in filePaths)
             {
@@ -163,6 +273,22 @@ namespace FRESHMusicPlayer.ViewModels.TagEditor
                 Composer = track.Composer;
                 TrackNumber = track.TrackNumber.ToString();
                 DiscNumber = track.DiscNumber.ToString();
+
+                int i = 1;
+                if (track.EmbeddedPictures.Count != 0)
+                {
+                    foreach (var cover in track.EmbeddedPictures)
+                    {
+                        AvailableCoverArts.Add(i.ToString());
+                        CoverArts.Add(cover);
+                        i++;
+                    }
+                }
+            }
+            if (CoverArts.Count != 0)
+            {
+                SelectedCoverArt = 0;
+                ChangeCoverArt();
             }
             UnsavedChanges = false; // override setting these usually making unsaved changes true, kinda jank but it works lol
         }
@@ -192,7 +318,7 @@ namespace FRESHMusicPlayer.ViewModels.TagEditor
                 },
                 AllowMultiple = true
             };
-            var files = await dialog.ShowAsync(GetMainWindow());
+            var files = await dialog.ShowAsync(Window);
             Initialize(files.ToList());
         }
 
@@ -227,11 +353,9 @@ namespace FRESHMusicPlayer.ViewModels.TagEditor
                 Library?.Remove(path); // update library entry, if available
                 Library?.Import(path);
             }
+            UnsavedChanges = false;
         }
 
-        public void ExitCommand()
-        {
-
-        }
+        public void ExitCommand() => Window.Close();
     }
 }
