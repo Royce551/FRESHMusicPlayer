4c02ae3f3ec28f6ee6789d8a3304972d60ebea09
Royce551 <poohwie2005@gmail.com>
2022-02-13 15:01:53 -0600

    Update copyright and version

--- FRESHMusicPlayer/FRESHMusicPlayer-WPF/Properties/AssemblyInfo.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer-WPF/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Squid Grill")]
 [assembly: AssemblyProduct("FRESHMusicPlayer")]
-[assembly: AssemblyCopyright("Copyright © Squid Grill 2021")]
+[assembly: AssemblyCopyright("Copyright © Squid Grill 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,6 +49,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("11.0.1.0")]
-[assembly: AssemblyFileVersion("11.0.1.0")]
+[assembly: AssemblyVersion("11.1.0.0")]
+[assembly: AssemblyFileVersion("11.1.0.0")]
 [assembly: AssemblyInformationalVersion("FRESHMusicPlayer Ver. 11")]
