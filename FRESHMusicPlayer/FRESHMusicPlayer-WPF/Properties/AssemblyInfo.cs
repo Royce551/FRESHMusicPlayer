c9500e5ec989400753c49196f5b999f4485537de
Royce551 <poohwie2005@gmail.com>
2022-03-19 15:30:04 -0500

    Update FMP Core; bump version

--- FRESHMusicPlayer/FRESHMusicPlayer-WPF/Properties/AssemblyInfo.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer-WPF/Properties/AssemblyInfo.cs
@@ -49,6 +49,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("11.1.3.0")]
-[assembly: AssemblyFileVersion("11.1.3.0")]
+[assembly: AssemblyVersion("11.1.4.0")]
+[assembly: AssemblyFileVersion("11.1.4.0")]
 [assembly: AssemblyInformationalVersion("FRESHMusicPlayer Ver. 11")]
