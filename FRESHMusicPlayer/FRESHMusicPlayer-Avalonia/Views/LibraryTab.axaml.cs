ca64bea4223086717939a078a818d7ad57f5f37d
Royce551 <poohwie2005@gmail.com>
2022-04-11 06:07:44 -0500

    Play/enqueue/delete buttons!

--- FRESHMusicPlayer/FRESHMusicPlayer-Avalonia/Views/LibraryTab.axaml.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer-Avalonia/Views/LibraryTab.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using FRESHMusicPlayer.ViewModels;
 
@@ -7,6 +9,8 @@ namespace FRESHMusicPlayer.Views
 {
     public partial class LibraryTab : UserControl
     {
+        private LibraryTabViewModel ViewModel => DataContext as LibraryTabViewModel;
+
         public LibraryTab()
         {
             InitializeComponent();
@@ -14,10 +18,37 @@ namespace FRESHMusicPlayer.Views
 
         public LibraryTab SetStuff(MainWindowViewModel mainWindowVm, Tab selectedTab, string initialSearch = null)
         {
-            var viewModel = DataContext as LibraryTabViewModel;
-            viewModel.MainWindowWm = mainWindowVm;
-            viewModel.Initialize(selectedTab, initialSearch);
+            ViewModel.MainWindowWm = mainWindowVm;
+            ViewModel.Initialize(selectedTab, initialSearch);
             return this;
+        }
+
+        private async void OnPlayButtonClick(object sender, RoutedEventArgs e)
+        {
+            var cmd = (Button)sender;
+            if (cmd.DataContext is DatabaseTrack x)
+            {
+                ViewModel.MainWindowWm.Player.Queue.Clear();
+                await ViewModel.MainWindowWm.Player.PlayAsync(x.Path);
+            }
+        }
+
+        private void OnEnqueueButtonClick(object sender, RoutedEventArgs e)
+        {
+            var cmd = (Button)sender;
+            if (cmd.DataContext is DatabaseTrack x)
+            {
+                ViewModel.MainWindowWm.Player.Queue.Add(x.Path);
+            }
+        }
+
+        private void OnDeleteButtonClick(object sender, RoutedEventArgs e)
+        {
+            var cmd = (Button)sender;
+            if (cmd.DataContext is DatabaseTrack x)
+            {
+                ViewModel.MainWindowWm.Library.Remove(x.Path);
+            }
         }
 
         private void InitializeComponent()
