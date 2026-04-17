2c61b1cc042baff1fc4aee20ced07bc4cf4e97c5
Royce551 <poohwie2005@gmail.com>
2026-04-17 00:26:18 -0500

    Update CI

--- Distribution/build-linux.sh
+++ Distribution/build-linux.sh
@@ -6,12 +6,12 @@ APPDIR=$"FRESHMusicPlayer.AppDir"
 rm -rf $APPDIR || true
 
 
-pushd FRESHMusicPlayer/FRESHMusicPlayer-Avalonia
-dotnet publish FRESHMusicPlayer-Avalonia.csproj -c Release -r linux-x64
+pushd FRESHMusicPlayer/FRESHMusicPlayer.Linux
+dotnet publish FRESHMusicPlayer.Linux.csproj -c Release -r linux-x64
 popd
 
 mkdir -p $APPDIR
-cp -r "FRESHMusicPlayer/FRESHMusicPlayer-Avalonia/bin/Release/net5.0/linux-x64/publish/"* $APPDIR
+cp -r "FRESHMusicPlayer/FRESHMusicPlayer.Linux/bin/Release/net10.0/linux-x64/publish/"* $APPDIR
 cp Distribution/io.github.royce551.FRESHMusicPlayer.desktop $APPDIR
 cp Distribution/logo.svg $APPDIR/io.github.royce551.FRESHMusicPlayer.svg
 
--- Distribution/build-macos.sh
+++ Distribution/build-macos.sh
@@ -9,12 +9,12 @@ RESOURCES_BUNDLE=$APP_BUNDLE/Contents/Resources
 
 rm -rf $APP_BUNDLE || true
 
-pushd FRESHMusicPlayer/FRESHMusicPlayer-Avalonia
+pushd FRESHMusicPlayer/FRESHMusicPlayer.MacOS
 dotnet publish -r osx-$BUILD_ARCH --self-contained
 popd
 
 mkdir -p $APP_BUNDLE/Contents/MacOS
-cp -r "FRESHMusicPlayer/FRESHMusicPlayer-Avalonia/bin/Debug/net"*"/osx-$BUILD_ARCH/"* $APP_BUNDLE/Contents/MacOS
+cp -r "FRESHMusicPlayer/FRESHMusicPlayer.MacOS/bin/Debug/net"*"/osx-$BUILD_ARCH/"* $APP_BUNDLE/Contents/MacOS
 mkdir -p $RESOURCES_BUNDLE
 cp Distribution/icon.icns $RESOURCES_BUNDLE
 cp Distribution/Info.plist $APP_BUNDLE/Contents/
