e38e018338cc984a5c93b31793d6ead9e1a7c5d9
Royce551 <poohwie2005@gmail.com>
2026-05-11 21:41:59 -0500

    One more try using App.axaml

--- FRESHMusicPlayer/FRESHMusicPlayer/App.axaml.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer/App.axaml.cs
@@ -34,7 +34,6 @@ public partial class App : Application
         //// Line below is needed to remove Avalonia data validation.
         //// Without this line you will get duplicate validations from both Avalonia and CT
         //BindingPlugins.DataValidators.RemoveAt(0);
-        Current?.Name = MainViewModel.WindowName;
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             var mainWindow = new MainWindow();
