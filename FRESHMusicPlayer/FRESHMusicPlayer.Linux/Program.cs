a91a95bc39aeda8d7080de7016e9aa2ed5fef5bc
Royce551 <poohwie2005@gmail.com>
2026-07-27 20:30:10 -0500

    Add disable wayland arg to Linux

--- FRESHMusicPlayer/FRESHMusicPlayer.Linux/Program.cs
+++ FRESHMusicPlayer/FRESHMusicPlayer.Linux/Program.cs
@@ -8,22 +8,30 @@ namespace FRESHMusicPlayer.Desktop;
 
 class Program
 {
+    private static bool disableWayland = false;
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        if (args.Contains("--disable-wayland")) disableWayland = true;
 
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
     {
         Locator.CurrentMutable.Register<IPlatformWrapper>(() => new LinuxPlatformWrapper());
-
-        return AppBuilder.Configure<App>()
+        
+        var appBuilder = AppBuilder.Configure<App>()
                 .UsePlatformDetect()
                 .WithInterFont()
-                .UseWayland()
                 .LogToTrace();
+
+        if (!disableWayland) appBuilder = appBuilder.UseWayland();
+
+        return appBuilder;
     }
 }
